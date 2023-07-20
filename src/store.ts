29f976986fc74740d7080afc592ece507c41c5b4
Joe Sun <dps_swanuk@hotmail.com>
2023-07-20 20:47:32 +0200

    add reducer

--- src/store.ts
+++ src/store.ts
@@ -5,5 +5,5 @@ export const store = configureStore({
     }
 });
 
-export type AppDispath = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
